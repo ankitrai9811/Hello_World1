bdbbb8b46abd742084605bc116acf9bc157acbec
ankitrai9811 <131332029+ankitrai9811@users.noreply.github.com>
2023-04-25 09:22:09 +0530

    Update HelloWorld1Application.java

--- src/main/java/com/example/demo/HelloWorld1Application.java
+++ src/main/java/com/example/demo/HelloWorld1Application.java
@@ -23,7 +23,7 @@ public class HelloWorld1Application extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        String s="This Is First Jenkins Deployment By Ankit Rai";
+        String s="1. This Is First Jenkins Deployment By Ankit Rai \n 2. Second Line";
         return s;
     }
 
